17640c799e540bbbd405d1f2be0ed6c14cf53bb9
aa055 <aamirmoiz531@outlook.com>
2025-12-01 22:42:43 +0400

    Updated the cv parser agent file

--- agents/cv_parser_agent.py
+++ agents/cv_parser_agent.py
@@ -1,14 +1,12 @@
-from langchain_community.document_loaders import PyPDFLoader
-from langchain.text_splitter import RecursiveCharacterTextSplitter
-from langchain.embeddings import OllamaEmbeddings
+from pypdf import PdfReader
+from langchain_text_splitters import RecursiveCharacterTextSplitter
 
 class CVParserAgent:
     def __init__(self):
         self.splitter = RecursiveCharacterTextSplitter(chunk_size=500, chunk_overlap=50)
 
     def parse_cv(self, pdf_path):
-        loader = PyPDFLoader(pdf_path)
-        pages = loader.load()
-        text = " ".join([p.page_content for p in pages])
+        reader = PdfReader(pdf_path)
+        text = " ".join([page.extract_text() for page in reader.pages])
         chunks = self.splitter.split_text(text)
         return {"text": text, "chunks": chunks}
